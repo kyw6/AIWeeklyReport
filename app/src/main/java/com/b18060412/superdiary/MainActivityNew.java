1dd20e5754ea9461b77c35e70e7bf95e68a0ae32
mk <2370553834@qq.com>
2024-08-21 20:21:35 +0800

    添加一个Activity，准备重构MainActivity

---
+++ app/src/main/java/com/b18060412/superdiary/MainActivityNew.java
@@ -0,0 +1,17 @@
+package com.b18060412.superdiary;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivityNew extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main_new);
+
+    }
+}
