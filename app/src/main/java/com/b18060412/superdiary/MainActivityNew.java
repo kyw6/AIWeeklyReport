0c339da59011821060702e86659c143b6aa1db54
makang <makang3@xiaomi.com>
2024-08-21 20:52:42 +0800

    初始化顶部文字

--- app/src/main/java/com/b18060412/superdiary/MainActivityNew.java
+++ app/src/main/java/com/b18060412/superdiary/MainActivityNew.java
@@ -1,17 +1,44 @@
 package com.b18060412.superdiary;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.haibin.calendarview.CalendarView;
+
 
 public class MainActivityNew extends AppCompatActivity {
+    private TextView tvYearMonth;
+    private CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main_new);
+        tvYearMonth = findViewById(R.id.tv_year_month);
+        calendarView = findViewById(R.id.CV_calendar);
+        initTopText();//初始化顶部文字显示
+    }
 
+    //初始化顶部文字显示
+    private void initTopText() {
+        // 初始化 TextView 显示当前月份
+        int year = calendarView.getCurYear();
+        int month = calendarView.getCurMonth();
+        updateYearMonthText(year, month);
+
+        // 设置月份变化监听器
+        calendarView.setOnMonthChangeListener(new CalendarView.OnMonthChangeListener() {
+            @Override
+            public void onMonthChange(int year, int month) {
+                updateYearMonthText(year, month);
+            }
+        });
+    }
+
+    private void updateYearMonthText(int year, int month) {
+        String yearMonth = year + "年" + month + "月";
+        tvYearMonth.setText(yearMonth);
     }
 }
