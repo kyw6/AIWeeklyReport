4fa113082b8b0cc9e5f77a10bf78e4c351fa50aa
makang <makang3@xiaomi.com>
2024-08-21 21:25:41 +0800

    首页日期点击事件设置

--- app/src/main/java/com/b18060412/superdiary/MainActivityNew.java
+++ app/src/main/java/com/b18060412/superdiary/MainActivityNew.java
@@ -5,12 +5,14 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.haibin.calendarview.Calendar;
 import com.haibin.calendarview.CalendarView;
 
 
 public class MainActivityNew extends AppCompatActivity {
     private TextView tvYearMonth;
-    private CalendarView calendarView;
+    private CalendarView calendarView;//日期
+    private TextView textViewReport;//周报展示区域
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +20,9 @@ public class MainActivityNew extends AppCompatActivity {
         setContentView(R.layout.activity_main_new);
         tvYearMonth = findViewById(R.id.tv_year_month);
         calendarView = findViewById(R.id.CV_calendar);
+        textViewReport = findViewById(R.id.textView_weekly_report);
         initTopText();//初始化顶部文字显示
+        initReportText();//初始化周报展示区域
     }
 
     //初始化顶部文字显示
@@ -35,6 +39,27 @@ public class MainActivityNew extends AppCompatActivity {
                 updateYearMonthText(year, month);
             }
         });
+    }
+
+    //初始化周报展示区域
+    private void initReportText() {
+        calendarView.setOnCalendarSelectListener(new CalendarView.OnCalendarSelectListener() {
+            @Override
+            public void onCalendarOutOfRange(Calendar calendar) {
+                // 当选中的日期超出范围时回调
+            }
+
+            @Override
+            public void onCalendarSelect(Calendar calendar, boolean b) {
+                // 当日期被选中时回调
+                int year = calendar.getYear();
+                int month = calendar.getMonth();
+                int day = calendar.getDay();
+                textViewReport.setText("今天是" + year + "年" + month + "月" + day + "日");
+            }
+        });
+
+
     }
 
     private void updateYearMonthText(int year, int month) {
