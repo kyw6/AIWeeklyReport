f41c7081d2cd357d7e03b0a2bcec3d06ac440a56
mk <2370553834@qq.com>
2024-08-25 18:16:15 +0800

    新增FirstActivity

--- app/src/main/java/com/b18060412/superdiary/util/PreferenceKeys.java
+++ app/src/main/java/com/b18060412/superdiary/util/PreferenceKeys.java
@@ -1,8 +1,6 @@
 package com.b18060412.superdiary.util;
 
 public class PreferenceKeys {
-    public static final String KEY_PRIVACY_ACCEPTED = "privacyAccepted";
-    public static final String KEY_LOGIN_TOKEN = "loginToken";
-    public static final String KEY_HOME_PAGE_DATA = "homePageData";
+    public static final String USER_UUID_KEY = "uuid";
 }
 
